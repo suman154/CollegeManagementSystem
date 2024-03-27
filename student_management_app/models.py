ae527c5f70e236e56d5a8f198bbae49167c01818
suman154 <sumanb@gmail.com>
2024-03-27 11:50:18 +0545

    .\college management system

--- student_management_app/models.py
+++ student_management_app/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class SessionYearModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    session_start_year = models.DateField()
+    session_end_year = models.DateField()
+    objects = models.Manager()
+
+
+
+class CustomUser(AbstractUser):
+    HOD = '1'
+    STAFF = '2'
+    STUDENT = '3'
+
+    EMAIL_TO_USER_TYPE_MAP = {
+        'hod': HOD,
+        'staff' : STAFF, 
+        'student': STUDENT
+    }
+
+
+    user_type_data = ((HOD, "HOD"), (STAFF, "Staff"), (STUDENT, "Student"))
+    user_type = models.CharField(default=1, choices=user_type_data, max_length=10)
+
+
+
+class AdminHOD(models.Model):
+    id =  models.AutoField(primary_key=True)
+    admin = models.OneToOneField(CustomUser, on_delete = models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now_add=True)
+    objects = models.manager()
+
+
+class Staffs(models.Model):
+    id = models.AutoField(primary_key=True)
+    admin = models.OneToOneField(CustomUser, on_delete = models.CASCADE)
+    address = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.AutoField(auto_now_add=True)
+    objects = models.Manager()
+
+
+
+
+
+class Course(models.Model):
+    id = models.AutoField(primary_key=True)
+    course_name = models.CharField(max_length=255)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now_add=True)
+    objects = models.Manager
+
+
+
+class subject(models.Model):
+    id = models.AutoField(primary_key=True)
+    subject_name = models.CharField(max_length=255)
+    
